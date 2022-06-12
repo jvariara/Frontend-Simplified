ef1390ecf772d15d77df074591feb68fdbe428e2
jvariara <jvariara@gmail.com>
2022-06-11 21:28:19 -0400

    Learned about CSS variables and transform

---
+++ Learning Crash Course/CSS Theme Toggler/script.js
@@ -0,0 +1,13 @@
+/* Used to get the value of a variable from style sheet
+console.log(window.getComputedStyle(document.documentElement)
+.getPropertyValue('--text-color'))
+*/
+document.getElementById('dark-theme-btn').addEventListener
+('click', () => {
+    document.documentElement.style.setProperty('--background-color', '#333')
+})
+
+document.getElementById('light-theme-btn').addEventListener
+('click', () => {
+    document.documentElement.style.setProperty('--background-color', '#fff')
+})
